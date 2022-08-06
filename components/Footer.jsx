ec6f8b92266addb7eabdf3d15cd85fe7851b21d6
Germán Alberto Valencia Torres <lenovopromotor@gmail.com>
2022-08-06 12:27:29 -0500

    ant3products

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,17 +4,31 @@ import styles from "../styles/Footer.module.css";
 function Footer() {
   return (
     <div className={styles.container}>
-      <div className={styles.carL}>
+      <div className={styles.cardL}>
         <h1 className={styles.title}>Advance Network Technolgy</h1>
         <h1 className={styles.linkTitle}>
           <a href="/contact" passHref>
-            <span>Trabaja con nosotros</span>
-            <Image src="/yahoo.png" width="40px" height="40px" alt="imagen" />
+            <span className={styles.linkText}>Trabaja con nosotros</span>
+            <Image src="/link.png" width="40px" height="40px" alt="imagen" />
           </a>
         </h1>
       </div>
-      <div className={styles.carS}></div>
-      <div className={styles.carS}></div>
+      <div className={styles.cardS}>
+        <div className={styles.cardItem}>
+          12 ADAM STREET NY <br /> USA
+        </div>
+        <div className={styles.cardItem}>
+          antsoftdesign@gmail.com <br /> +57 315 745 62 80
+        </div>
+      </div>
+      <div className={styles.cardS}>
+        <div className={styles.cardItem}>
+          SÍGUENOS: <br /> __FB __IN __BE __TW
+        </div>
+        <div className={styles.cardItem}>
+          © 2022 ANT <br /> TODOS LOS DERECHOS RESERVADOS
+        </div>
+      </div>
     </div>
   );
 }
