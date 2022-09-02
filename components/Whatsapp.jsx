f389b2c80dd292934b543ec80a79415222abbca6
Germán Alberto Valencia Torres <lenovopromotor@gmail.com>
2022-09-02 09:46:15 -0500

    ant3footer11

--- components/Whatsapp.jsx
+++ components/Whatsapp.jsx
@@ -8,8 +8,9 @@ function Whatsapp() {
     <div className={styles.container}>
       <a
         className={styles.img}
-        href="https://api.whatsapp.com/send?phone=+573157456280&amp;text=Hola!%20Me%20comunico%20desde%20la%20p%C3%A1gina%20web%20para"
-        rel="noreferrer nofollow" target="_blank"
+        href="https://api.whatsapp.com/send?phone=+542616094179&amp;text=Hola!%20Me%20comunico%20desde%20la%20p%C3%A1gina%20web%20para"
+        rel="noreferrer nofollow"
+        target="_blank"
       >
         <Image src="/whatsapp.png" alt="whatsapp" width="80%" height="80%" />
       </a>
