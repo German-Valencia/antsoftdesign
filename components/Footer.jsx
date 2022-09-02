bf74e1ba3cb64a869bfd90bfc1316c373ac15733
Germán Alberto Valencia Torres <lenovopromotor@gmail.com>
2022-09-02 10:30:57 -0500

    ant3footer13

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -11,23 +11,26 @@ function Footer() {
         <h1 className={styles.linkTitle}>
           <Link href="/contact" passHref>
             <a>
-            <span className={styles.linkText}>Trabaja con nosotros</span>
-            <Image src="/link.png" width="30px" height="30px" alt="imagen" />
+              <span className={styles.linkText}>Trabaja con nosotros</span>
+              <Image src="/link.png" width="30px" height="30px" alt="imagen" />
             </a>
           </Link>
         </h1>
       </div>
       <div className={styles.cardS}>
         <div className={styles.cardItem}>
-          Argentina<br />+54 261 609 4179<br />  <br />Colombia <br />+57 315 745 6280
-        </div>
-        <div className={styles.cardItem}>
-         <a href="mailto:antsoftdesign@gmail.com">antsoftdesign@gmail.com</a> 
+          Argentina: +54 261 609 4179
+          <br />
+          <br />
+          Colombia: +57 315 745 6280 <br />
+          <br />
+          <a href="mailto:antsoftdesign@gmail.com">antsoftdesign@gmail.com</a>
         </div>
       </div>
       <div className={styles.cardS}>
         <div className={styles.cardItem}>
-          Síguenos: <br />__FB __IN __TW
+          Síguenos: <br />
+          __FB __IN __TW
         </div>
         <div className={styles.cardItem}>
           © 2022 ANT <br /> Todos los derechos reservados
